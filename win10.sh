94815cacb6b6453489337acd444a0b7c732a48f5
bonggolo <barusbolang4@gmail.com>
2024-11-01 02:03:00 +0700

    Update win10.sh

--- win10.sh
+++ win10.sh
@@ -40,7 +40,7 @@ case $choice in
     3)
         # Windows Server 2022
         img_file="windows2022.img"
-        iso_link="https://nobar21.lol/Windows.iso"
+        iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
         iso_file="windows2022.iso"
         ;;
     *)
